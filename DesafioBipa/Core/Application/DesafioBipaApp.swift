56ce59fb41cbf27c84924fe3e34da15a7361e7be
tiagocomth <thiago8jesusaraujo@gmail.com>
2026-01-09 11:40:14 -0300

    Initial Commit

---
+++ DesafioBipa/Core/Application/DesafioBipaApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesafioBipaApp.swift
+//  DesafioBipa
+//
+//  Created by Thiago de Jesus on 09/01/26.
+//
+
+import SwiftUI
+
+@main
+struct DesafioBipaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
---
+++ DesafioBipa/Features/Home/View/Components/HeaderBipa.swift
@@ -0,0 +1,18 @@
+//
+//  HeaderBipa.swift
+//  DesafioBipa
+//
+//  Created by Thiago de Jesus on 09/01/26.
+//
+
+import SwiftUI
+
+struct HeaderBipa: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HeaderBipa()
+}
